a5b1aa6d872c7b7aa193f5dd3b34031aae2a4f7d
Ana Júlia Leite <leite.anajulia25@gmail.com>
2025-09-09 00:25:21 -0300

    Atualização
    Subindo exercícios valendo nota de trabalho

---
+++ OAT1/EX05/EX05.py
@@ -0,0 +1,17 @@
+﻿# -*- coding: utf-8 -*-
+
+# Exercicio 5
+class Animal:
+    def falar(self):
+        print("O animal faz um som.")
+
+class Cachorro(Animal):
+    def falar(self):
+        print("Au Au 🐶")
+
+a = Animal()
+a.falar()
+
+c = Cachorro()
+c.falar()
+
