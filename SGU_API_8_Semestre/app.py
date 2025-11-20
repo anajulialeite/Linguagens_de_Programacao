9bfb3e954ad9da197f40abb73d4a76f69757fe7d
Ana Júlia Leite <leite.anajulia25@gmail.com>
2025-11-20 04:10:54 -0300

    Atualização
    Ajustando o Init.py, o app.py e instalando bibliotecas usando o comando pip install

--- SGU_API_8_Semestre/app.py
+++ SGU_API_8_Semestre/app.py
@@ -1,9 +1,4 @@
 from src import app
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-
     app.run()
-=======
-    app.run()
->>>>>>> meu-trabalho
--- SGU_API_8_Semestre/src/__init__.py
+++ SGU_API_8_Semestre/src/__init__.py
@@ -10,10 +10,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object('connection')
-<<<<<<< HEAD
-=======
 
->>>>>>> meu-trabalho
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 ma = Marshmallow(app)
@@ -23,15 +20,5 @@ api = Api(app)
 from src.models.usuario_model import UsuarioModel
 from src.models.tarefa_model import TarefaModel
 
-<<<<<<< HEAD
-
-#TODO: Apontar os modelos (tabelas)
-#      Apontar as Views
-
-from .models.usuario_model import UsuarioModel
-
-from .views import usuario_view
-=======
 # importa as views (elas mesmas registram as rotas)
 from src.views import usuario_view, tarefa_view
->>>>>>> meu-trabalho
